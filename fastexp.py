aaf776e9f59f289700808f9ec9948107b27d9e9f
Parker Collier <pcollie4@vol.utk.edu>
2022-10-12 12:17:41 -0400

    start fast

---
+++ fastexp.py
@@ -0,0 +1,10 @@
+def fastexp(b: int, e: int, n: int) -> int:
+    pass
+
+if __name__ == "__main__":
+    b = 4235880211405804673
+    e = 131
+    n = 12855544647099734480
+
+    res = fastexp(b, e, n)
+    print(res)
