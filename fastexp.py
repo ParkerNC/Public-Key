7ca4cf29cb17401d082e63fa0a69a75bb44c08fd
string <pcollie4@vols.utk.edu>
2022-10-13 21:59:59 -0400

    makin primes

--- fastexp.py
+++ fastexp.py
@@ -2,7 +2,7 @@ from random import randrange
 from random import getrandbits
 
 # Pre generated primes
-first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
                      31, 37, 41, 43, 47, 53, 59, 61, 67,
                      71, 73, 79, 83, 89, 97, 101, 103,
                      107, 109, 113, 127, 131, 137, 139,
@@ -16,10 +16,12 @@ def fastexp(b: int, e: int, n: int) -> int:
     prod = 1
     base = b
     exp = bin(e)[::-1]
+    exp = exp[:-2]
     for bit in exp:
         if bit == "1":
-            prod = ((prod % n) * (base % n)) %n
-        base = ((base % n) * (base % n)) %n
+            prod = (prod * base) %n
+        
+        base = (base * base ) %n
 
     return prod
 
@@ -52,15 +54,23 @@ def millerR(p: int, i: int) -> bool:
 
 
 def primeGen(bitlen: int) -> int:
+    qlen = bitlen-1
     while(1):
-        test = randrange(2**(bitlen-1)+1, 2**bitlen-1)
+        test = randrange(2**(qlen-1)+1, 2**qlen-1)
         if not test & 1:
             continue
 
-        if millerR(test, 10):
-            if millerR((test-1)>>1, 10):
-                return test
+        for prime in primelist:
+            if test % prime == 0:
+                continue
 
+
+        if millerR(test, 10):
+            test = (test << 1) + 1
+
+            if millerR(test, 10):
+                print(test)
+                return test
 
     return test
 
@@ -74,6 +84,5 @@ if __name__ == "__main__":
     #res = fastexp(b, e, n)
     tmp = millerR(175118011353974613747414115745586823026539540640072921440880723823673023327752553018161242200813211305556829955825029953131751251477020252777664395370693866564432656651061844109313776792589236703517908150685842487470642697808561061731352854662829518025487893728617216219673621722830025917527599882791614689407, 20)
 
-    print(tmp)
     x = primeGen(1024)
-    print(x)
+    #print(x)
